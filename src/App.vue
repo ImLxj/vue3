a5f79bc460c10587d72827f5b97208328cb5063b
李绪杰 <1340482172@qq.com>
2022-04-27 16:46:17 +0800

    vue3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
-  <Student v-if="isDisplay"></Student>
-  <button @click="isDisplay = !isDisplay">点击隐藏组件</button>
+  <h2>{{ keyWord }}</h2>
+  <input type="text" v-model="keyWord" />
 </template>
 
 <script>
-import Student from '@/components/Student'
-import { ref } from 'vue'
+import { customRef, ref } from 'vue'
 export default {
   name: 'App',
-  components: { Student },
   setup() {
-    let isDisplay = ref(true)
+    // 自定义一个ref名为myRef
+    function myRef(value, delay) {
+      let timer
+      return customRef((track, trigger) => {
+        return {
+          get() {
+            track() // 通知vue最终value的变化
+            return value
+          },
+          set(newValue) {
+            clearTimeout()
+            timer = setTimeout(() => {
+              value = newValue
+              trigger() // 通知vue去重新解析模板
+            }, delay)
+          }
+        }
+      })
+    }
+
+    let keyWord = myRef('hello', 1000)
 
     return {
-      isDisplay
+      keyWord
     }
   }
 }
