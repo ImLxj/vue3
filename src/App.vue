60af8049e6620ef0e68106c9cbf8f1e96ddeaea7
李绪杰 <1340482172@qq.com>
2022-04-27 12:06:44 +0800

    MASTER

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>一个人的信息</h1>
+  <h2>姓名:{{ person.name }}</h2>
+  <h2>年龄:{{ person.age }}</h2>
+  <h3>工作岗位:{{ person.job.type }}</h3>
+  <h3>薪资:{{ person.job.salary }}</h3>
+  <h3>爱好:{{ person.hobby }}</h3>
+  <button @click="sayPerson">点我修改人的信息</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive } from 'vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    // 数据
+    let person = reactive({
+      name: '张三',
+      age: 15,
+      job: {
+        type: '前端工程师',
+        salary: '100k'
+      },
+      hobby: ['抽烟', '喝酒', '烫头']
+    })
+    function sayPerson() {
+      person.name = '李四'
+      person.age = 20
+      person.job.type = 'Java工程师'
+      person.hobby[0] = '学习'
+    }
+
+    // 返回一个对象(常用)
+    return {
+      person,
+      sayPerson
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
